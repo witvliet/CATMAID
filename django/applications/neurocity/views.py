708d8b1337068e3c7f8142ddd8c94e53a68e9972
Stephan Gerhard <git@unidesign.ch>
2013-05-11 23:17:12 -0400

    Updates

--- django/applications/neurocity/views.py
+++ django/applications/neurocity/views.py
@@ -51,7 +51,6 @@ def userstatistics_view(request):
         'matches': matches
         }, context_instance=RequestContext(request))
 
-
 def profile_view(request):
     if request.method == 'POST':
         form = UserForm(instance=request.user, data=request.POST)
@@ -70,7 +69,6 @@ def profile_view(request):
         'flag': request.user.userprofile.country.code.lower()
     })
 
-
 def language_view(request):
     return render_to_response('neurocity/setlanguage.html', {
         }, context_instance=RequestContext(request))
@@ -83,101 +81,63 @@ def terms_view(request):
     return render_to_response('neurocity/terms.html', {
         }, context_instance=RequestContext(request))
 
- # def contact_view(request):
- #     return render_to_response('neurocity/contact.html', {
- #         }, context_instance=RequestContext(request))
+def home_view(request):
+    context = {}
+    context['nc_home_active'] = 'active'
+    context['GOOGLE_TRACKING_ID'] = settings.GOOGLE_TRACKING_ID
+    return render(request, 'neurocity/home.html', context)
 
-class NeurocityBaseView(TemplateView):
+def tutorial_view(request):
+    context = {}
+    context['nc_tutorial_active'] = 'active'
+    context['GOOGLE_TRACKING_ID'] = settings.GOOGLE_TRACKING_ID
+    return render(request, 'neurocity/tutorial.html', context)
 
-    def get_context_data(self, **kwargs):
-        context = super(NeurocityBaseView, self).get_context_data(**kwargs)
-        # context['flag'] = self.request.user.userprofile.country.code.lower()
-        context['GOOGLE_TRACKING_ID'] = settings.GOOGLE_TRACKING_ID
-        return context
+def learn_view(request):
+    context = {}
+    context['nc_learn_active'] = 'active'
+    context['GOOGLE_TRACKING_ID'] = settings.GOOGLE_TRACKING_ID
+    return render(request, 'neurocity/learn.html', context)
 
-class NeurocityHomeView(NeurocityBaseView):
+def leaderboard_view(request):
+    context = {}
+    context['nc_dashboard_active'] = 'active'
+    context['flag'] = request.user.userprofile.country.code.lower()
+    context['GOOGLE_TRACKING_ID'] = settings.GOOGLE_TRACKING_ID
+    # context['sv_count'] = SegmentVote.objects.filter(
+    #     creation_time__gte=datetime.date.today(),
+    #     creation_time__lt=datetime.date.today()+datetime.timedelta(days=1)
+    # ).count()
 
-    template_name = "neurocity/home.html"
+    daily_vote_count = SegmentVote.objects.filter(
+        creation_time__gte=date.today(),
+        creation_time__lt=date.today()+timedelta(days=1)
+    ).values('user', 'user__username', 'user__userprofile__country').annotate(uc = Count('user')).order_by('uc')
+    result_score = []
+    for i, q in enumerate(daily_vote_count):
+        result_score.append((i+1, q['user__username'], q['user__userprofile__country'].lower(), q['uc']) )
+    context['result_score'] = result_score
 
-    def get_context_data(self, **kwargs):
-        context = super(NeurocityHomeView, self).get_context_data(**kwargs)
-        # context['flag'] = self.request.user.userprofile.country.code.lower()
-        context['nc_home_active'] = 'active'
-        return context
+    return render(request, 'neurocity/dashboard.html', context)
 
-class TutorialView(NeurocityBaseView):
+def segmentonly_view(request):
+    context = {}
+    context['nc_segmentonly'] = True
+    segmentkey = int( request.GET.get('segmentkey', '0') )
+    segment = get_segment_by_key( segmentkey )
+    context['originsection'] = segment.origin_section
+    context['targetsection'] = segment.target_section
+    context['segmentid'] = segment.segmentid
+    context['segmentkey'] = segment.id
+    context['tile_base_url'] = 'http://localhost/datastatic/stack2/raw/'
+    context['cost'] = segment.cost
+    if segment.cost != 0.0:
+        context['aiguess'] = '%0.2f' % (1./segment.cost)
+    else:
+        context['aiguess'] = 0.0
+    return render(request, 'neurocity/contribute.html', context)
 
-    template_name = "neurocity/tutorial.html"
-
-    def get_context_data(self, **kwargs):
-        context = super(TutorialView, self).get_context_data(**kwargs)
-        context['nc_tutorial_active'] = 'active'
-        return context
-
-class LearnView(NeurocityBaseView):
-
-    template_name = "neurocity/learn.html"
-
-    def get_context_data(self, **kwargs):
-        context = super(LearnView, self).get_context_data(**kwargs)
-        context['nc_learn_active'] = 'active'
-        return context
-
-class DashboardView(NeurocityBaseView):
-
-    template_name = "neurocity/dashboard.html"
-
-
-    def get_context_data(self, **kwargs):
-        context = super(DashboardView, self).get_context_data(**kwargs)
-
-        context['nc_dashboard_active'] = 'active'
-        context['flag'] = self.request.user.userprofile.country.code.lower()
-        
-        # context['sv_count'] = SegmentVote.objects.filter(
-        #     creation_time__gte=datetime.date.today(),
-        #     creation_time__lt=datetime.date.today()+datetime.timedelta(days=1)
-        # ).count()
-
-        daily_vote_count = SegmentVote.objects.filter(
-            creation_time__gte=date.today(),
-            creation_time__lt=date.today()+timedelta(days=1)
-        ).values('user', 'user__username', 'user__userprofile__country').annotate(uc = Count('user')).order_by('uc')
-        result_score = []
-        for i, q in enumerate(daily_vote_count):
-            result_score.append((i+1, q['user__username'], q['user__userprofile__country'].lower(), q['uc']) )
-        context['result_score'] = result_score
-
-        # context['nc_users'] = User.objects.all().count()
-
-        return context
-
-class SegmentOnlyView(NeurocityBaseView):
-
-    template_name = "neurocity/contribute.html"
-
-    def get_context_data(self, **kwargs):
-        context = super(SegmentOnlyView, self).get_context_data(**kwargs)
-
-        context['nc_segmentonly'] = True
-        segmentkey = int( self.request.GET.get('segmentkey', '0') )
-        print 'segmentkey retrieved', segmentkey
-        segment = get_segment_by_key( segmentkey )
-        # segment = get_random_segment() # TODO: get particular segment
-        context['originsection'] = segment.origin_section
-        context['targetsection'] = segment.target_section
-        context['segmentid'] = segment.segmentid
-        context['segmentkey'] = segment.id
-        context['tile_base_url'] = 'http://localhost/datastatic/stack2/raw/'
-        context['cost'] = segment.cost
-        if segment.cost != 0.0:
-            context['aiguess'] = '%0.2f' % (1./segment.cost)
-        else:
-            context['aiguess'] = 0.0
-            
-        return context
-
-@ratelimit(ip=True, method=None, rate='5/m')
+@ratelimit(ip=True, method=None, rate='50/m')
 def contribute_view(request):
     if request.limited:
         return redirect('maxlimit')
@@ -191,7 +151,6 @@ def contribute_view(request):
     }, context_instance=RequestContext(request))
 
 def contact_view(request):
-
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
@@ -203,6 +162,3 @@ def contact_view(request):
     return render(request, 'neurocity/contact.html', {
         'form': form,
     })
-
-
-        
